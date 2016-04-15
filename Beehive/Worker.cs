2e679a8501a52dda013d33bcbe61bd697c8842d5
Andris Stalbovs <Andris.Stalbovs@squaliocc.com>
2016-04-15 10:14:29 +0300

    continue

--- Beehive/Worker.cs
+++ Beehive/Worker.cs
@@ -8,29 +8,54 @@ namespace Beehive
 {
     class Worker
     {
-       
-        private string[] JobsICanDo;
-        private bool busy { get; set; }
+
+        private string[] jobsICanDo;
+        public string CurrentJob
+        {
+            get
+            {
+
+                return "";
+            }
+        }
+        private int shiftsToWork { get; set; }
+        private int shiftsWorked { get; set; }
 
         public Worker(string[] jobICanDO)
         {
-            this.JobsICanDo = jobICanDO;
+            this.jobsICanDo = jobICanDO;
         }
 
         internal bool DoThisJob(string jobToDo, int shiftsCount)
         {
             //TODO: check if able to do job
-            if (busy)
-                return false;            
+            if (!string.IsNullOrEmpty(CurrentJob))
+                return false;
             else
             {
-                foreach (string item in JobsICanDo)
+                foreach (string item in jobsICanDo)
                 {
                     if (item.Contains(jobToDo))
+                    {
+
                         return true;
+                    }
+
                 }
                 return false;
-            }            
+            }
+        }
+
+        public bool DidYouFinish()
+        {
+            this.shiftsWorked++;
+            this.shiftsToWork--;
+            if (shiftsToWork<=0)
+            {
+                CurrentJob = ""; //Hooow???
+                return true;
+            }
+            return false;
         }
     }
 }
